5b454c54f8f169e3d967af63206c3caee82d19e9
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-01-05 19:36:04 -0300

    Change implementation of randomNumberInFactorRange function

--- src/index.ts
+++ src/index.ts
@@ -441,16 +441,24 @@ function getFirstOrLastRowIfDead(rows: AlienSet["aliens"]): number | null {
 
 /**
  * Generates a number which is a random number between
- * the given number multiplied by `1 - factor` and the given
- * number multiplied by `1 + factor`.
+ * the given number multiplied by `1 - subtractingFactor` and the given
+ * number multiplied by `1 + addingFactor`.
  *
- * @param n - A number
- * @param factor - The number that will be subtracted from and
- * added to 1 to generate a random number between the resulting
- * range.
+ * @param n - A number.
+ * @param subtractingFactor - The number that will be subtracted from 1.
+ * The result of the subtraction is multiplied by n and the result is the 
+ * minimum value of the range.
+ * @param addingFactor - The number that will be added to 1.
+ * The result of the addition is multiplied by n and the result is the 
+ * maximum value of the range.
+ * @returns - A random number between the resulting range.
  */
-function randomNumberInFactorRange(n: number, factor: number) {
-  return randomNum((1 - factor) * n, (1 + factor) * n);
+function randomNumberInFactorRange(
+  n: number,
+  subtractingFactor: number,
+  addingFactor = subtractingFactor
+) {
+  return randomNum((1 - subtractingFactor) * n, (1 + addingFactor) * n);
 }
 
 /* ==================================================================== */
